f1d97cafec3b96434210fb33125c5e3865169a56
towficaziz <tapolly01@gmail.com>
2022-11-11 00:22:44 +0100

    Add cart.service, work on cart compoent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'products', pathMatch:'full'},
+  {path:'products', component: ProductsComponent},
+  {path:'cart', component: CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
