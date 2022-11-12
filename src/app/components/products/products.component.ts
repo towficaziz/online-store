40be2c64b39597eab657c5b802a1984dbdfc062c
towficaziz <tapolly01@gmail.com>
2022-11-12 23:13:20 +0100

    Search on category

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -10,6 +10,7 @@ import { ApiService } from 'src/app/service/api.service';
 export class ProductsComponent implements OnInit {
 
   public productList: any;
+  public filterCategory: any;
   searchKey:string="";
   constructor(private api: ApiService, private cartService: CartService) { }
 
@@ -17,10 +18,14 @@ export class ProductsComponent implements OnInit {
     this.api.getProduct()
     .subscribe(res=>{
       this.productList= res;
-
+      this.filterCategory= res;
       this.productList.forEach((a:any) => {
+        if(a.category === "men's clothing" || a.category ==="women's clothing"){
+          a.category ="fashion"
+        }
         Object.assign(a,{quantity:1, total:a.price});
       });
+      console.log(this.productList);
     });
     this.cartService.search.subscribe((val:any)=>{
       this.searchKey = val;
@@ -29,6 +34,15 @@ export class ProductsComponent implements OnInit {
 
   addtoCart(item:any){
     this.cartService.addtoCart(item);
+  }
+
+  filter(category: string){
+    this.filterCategory = this.productList
+    .filter((a:any)=>{
+      if(a.category == category || category == ''){
+        return a;
+      }
+    })
   }
 
 }
